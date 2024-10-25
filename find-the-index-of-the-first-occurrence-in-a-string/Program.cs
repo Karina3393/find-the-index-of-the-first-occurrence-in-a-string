65102a95634e54268d23f0aeeb3418e572d1249f
карина <@DESKTOP-FP9I0KA>
2024-10-25 13:02:42 +0300

    Add project files.

---
+++ find-the-index-of-the-first-occurrence-in-a-string/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace find_the_index_of_the_first_occurrence_in_a_string
+{
+    public class Solution
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            if (needle == "") return 0;
+            return haystack.IndexOf(needle);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+           
+            (string haystack, string needle)[] testCases = {
+            ("hello", "ll"),
+            ("aaaaa", "bba"),
+            ("", ""),
+            ("abc", "c"),
+            ("mississippi", "issip")
+        };
+
+            foreach (var (haystack, needle) in testCases)
+            {
+                int result = solution.StrStr(haystack, needle);
+                Console.WriteLine($"haystack: \"{haystack}\", needle: \"{needle}\" -> result: {result}");
+            }
+        }
+    }
+}
